9d0ab884dea9657c86fbb7ae29382013145b21f2
Christian M <chriamue@gmail.com>
2022-12-22 17:19:38 +0100

    adds money example, pins imageproc dependency

--- src/fast.rs
+++ src/fast.rs
@@ -1,25 +1,25 @@
-use image::{ImageError, Rgba, GrayImage};
+use cgmath::{prelude::*, Rad};
+use image::{GrayImage, ImageError, Rgba};
 use imageproc::drawing::draw_line_segment_mut;
-use cgmath::{prelude::{*}, Rad};
 
 use crate::common;
 use common::*;
 
 // Consts
-const DEFAULT_FAST_THRESHOLD:i32 = 50;
+const DEFAULT_FAST_THRESHOLD: i32 = 50;
 
 #[derive(Debug, Clone, Copy)]
 pub struct FastKeypoint {
     pub location: Point,
     pub score: i32,
     pub nms_dist: usize,
-    pub moment: Moment
+    pub moment: Moment,
 }
 
 impl Matchable for FastKeypoint {
     fn distance(&self, other: &FastKeypoint) -> usize {
         let ((ax, ay), (bx, by)) = (self.location, other.location);
-        ((ax-bx).pow(2) as f32 + (ay-by).pow(2) as f32).sqrt() as usize
+        ((ax - bx).pow(2) as f32 + (ay - by).pow(2) as f32).sqrt() as usize
     }
 }
 
@@ -29,14 +29,14 @@ pub struct FastContext {
     idx: Vec<usize>,
     cmp: Vec<i32>,
     radius: u32,
-    n: u32
+    n: u32,
 }
 
 #[allow(non_camel_case_types)]
 #[derive(Debug, PartialEq)]
 pub enum FastType {
     TYPE_7_12,
-    TYPE_9_16
+    TYPE_9_16,
 }
 
 impl FastType {
@@ -44,32 +44,57 @@ impl FastType {
         match self {
             FastType::TYPE_7_12 => FastContext {
                 offsets: vec![
-                    ( 0, -2), ( 1, -2), ( 2, -1), ( 2,  0),
-                    ( 2,  1), ( 1,  2), ( 0,  2), (-1,  2),
-                    (-2,  1), (-2,  0), (-2, -1), (-1, -2)
+                    (0, -2),
+                    (1, -2),
+                    (2, -1),
+                    (2, 0),
+                    (2, 1),
+                    (1, 2),
+                    (0, 2),
+                    (-1, 2),
+                    (-2, 1),
+                    (-2, 0),
+                    (-2, -1),
+                    (-1, -2),
                 ],
                 idx: vec![0, 6, 3, 9, 1, 2, 4, 5, 7, 8, 10, 11],
                 cmp: vec![1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3],
                 radius: 3,
-                n: 9
+                n: 9,
             },
             FastType::TYPE_9_16 => FastContext {
                 offsets: vec![
-                    (0, -3), (1,  -3), (2, - 2), (3,  -1),
-                    (3,  0), (3,   1), (2,   2), (1,   3),
-                    (0,  3), (-1,  3), (-2,  2), (-3,  1),
-                    (-3, 0), (-3, -1), (-2, -2), (-1, -3) 
+                    (0, -3),
+                    (1, -3),
+                    (2, -2),
+                    (3, -1),
+                    (3, 0),
+                    (3, 1),
+                    (2, 2),
+                    (1, 3),
+                    (0, 3),
+                    (-1, 3),
+                    (-2, 2),
+                    (-3, 1),
+                    (-3, 0),
+                    (-3, -1),
+                    (-2, -2),
+                    (-1, -3),
                 ],
                 idx: vec![0, 8, 4, 12, 1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15],
                 cmp: vec![1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
                 radius: 4,
-                n: 12
-            }
+                n: 12,
+            },
         }
     }
 }
 
-pub fn fast(img: &image::GrayImage, fast_type: Option<FastType>, threshold: Option<i32>) -> Result<Vec<FastKeypoint>, ImageError> {
+pub fn fast(
+    img: &image::GrayImage,
+    fast_type: Option<FastType>,
+    threshold: Option<i32>,
+) -> Result<Vec<FastKeypoint>, ImageError> {
     let threshold = threshold.unwrap_or(DEFAULT_FAST_THRESHOLD);
     let fast_type = fast_type.unwrap_or(FastType::TYPE_9_16);
 
@@ -77,21 +102,26 @@ pub fn fast(img: &image::GrayImage, fast_type: Option<FastType>, threshold: Opti
 
     let mut fast_keypoint_matches = Vec::<FastKeypoint>::new();
 
-    for y in ctx.radius .. img.height()-ctx.radius {
-        'x_loop: for x in ctx.radius .. img.width()-ctx.radius {
+    for y in ctx.radius..img.height() - ctx.radius {
+        'x_loop: for x in ctx.radius..img.width() - ctx.radius {
             let center_pixel = img.get_pixel(x, y).0[0] as i32;
             let x = x as i32;
             let y = y as i32;
-            let point:Point = (x, y);
+            let point: Point = (x, y);
 
-            let mut score:i32 = 0;
-            let mut similars:i32 = 0;
+            let mut score: i32 = 0;
+            let mut similars: i32 = 0;
 
             for idx in 0..ctx.offsets.len() {
                 let px_idx = ctx.idx[idx];
-                let px = img.get_pixel((x + ctx.offsets[px_idx].0) as u32, (y + ctx.offsets[px_idx].1) as u32).0[0] as i32;
+                let px = img
+                    .get_pixel(
+                        (x + ctx.offsets[px_idx].0) as u32,
+                        (y + ctx.offsets[px_idx].1) as u32,
+                    )
+                    .0[0] as i32;
                 let diff = (px - center_pixel).abs();
-                
+
                 if diff < threshold {
                     similars += 1;
                     if similars > ctx.cmp[idx] {
@@ -107,7 +137,7 @@ pub fn fast(img: &image::GrayImage, fast_type: Option<FastType>, threshold: Opti
                 location: point,
                 score: score,
                 nms_dist: 0,
-                moment: moment
+                moment: moment,
             });
         }
     }
@@ -125,24 +155,31 @@ pub fn fast(img: &image::GrayImage, fast_type: Option<FastType>, threshold: Opti
 pub struct Moment {
     pub centroid: Point,
     pub moment: Point,
-    pub rotation: f64
+    pub rotation: f64,
 }
 
-fn patch_moment(img: &GrayImage, x:u32, y:u32, x_moment:u32, y_moment:u32, radius:Option<u32>) -> f64 {
+fn patch_moment(
+    img: &GrayImage,
+    x: u32,
+    y: u32,
+    x_moment: u32,
+    y_moment: u32,
+    radius: Option<u32>,
+) -> f64 {
     let radius = radius.unwrap_or(5);
 
     if x < radius || y < radius || x + radius >= img.width() || y + radius >= img.height() {
         return 1.0;
     }
 
-    let mut patch_sum:u32 = 0;
-    for mx in (x-radius)..=(x+radius) {
-        for my in (y-radius)..=(y+radius) {
+    let mut patch_sum: u32 = 0;
+    for mx in (x - radius)..=(x + radius) {
+        for my in (y - radius)..=(y + radius) {
             let coefficient = match (x_moment, y_moment) {
                 (0, 0) => 1,
                 (0, 1) => my,
                 (1, 0) => mx,
-                _ => 0
+                _ => 0,
             };
             patch_sum += coefficient * img.get_pixel(mx, my).0[0] as u32;
         }
@@ -151,15 +188,12 @@ fn patch_moment(img: &GrayImage, x:u32, y:u32, x_moment:u32, y_moment:u32, radiu
     patch_sum as f64
 }
 
-fn moment_centroid(img: &GrayImage, point: &Point, moment_radius:Option<u32>) -> Moment {
+fn moment_centroid(img: &GrayImage, point: &Point, moment_radius: Option<u32>) -> Moment {
     let p_m = patch_moment(img, point.0 as u32, point.1 as u32, 0, 0, moment_radius);
     let p_x = patch_moment(img, point.0 as u32, point.1 as u32, 1, 0, moment_radius);
     let p_y = patch_moment(img, point.0 as u32, point.1 as u32, 0, 1, moment_radius);
 
-    let (mx, my) = (
-        (p_x/p_m),
-        (p_y/p_m)
-    );
+    let (mx, my) = ((p_x / p_m), (p_y / p_m));
 
     let x_diff = (point.0 as f64 - mx) as f64;
     let y_diff = (point.1 as f64 - my) as f64;
@@ -167,7 +201,7 @@ fn moment_centroid(img: &GrayImage, point: &Point, moment_radius:Option<u32>) ->
     Moment {
         centroid: point.clone(),
         moment: (mx.round() as i32, my.round() as i32),
-        rotation: y_diff.atan2(x_diff)
+        rotation: y_diff.atan2(x_diff),
     }
 }
 
@@ -185,22 +219,20 @@ pub fn draw_moments(img: &mut image::RgbaImage, vec: &Vec<FastKeypoint>) {
 
         let end_point = (
             start_point.0 as f32 + (dist * Rad::cos(rotation_radians)).round() as f32,
-            start_point.1 as f32 + (dist * Rad::sin(rotation_radians)).round() as f32
+            start_point.1 as f32 + (dist * Rad::sin(rotation_radians)).round() as f32,
         );
 
         draw_line_segment_mut(
             img,
             (start_point.0 as f32, start_point.1 as f32),
             end_point,
-            Rgba([0, 0, 0, 125])
+            Rgba([0, 0, 0, 125]),
         );
 
         // draw circle
-        ctx.offsets
-            .clone()
-            .into_iter()
-            .for_each(|(dx, dy)| {
-                img.get_pixel_mut((k.location.0 + dx) as u32, (k.location.1 + dy) as u32).0 = color;
-            });
+        ctx.offsets.clone().into_iter().for_each(|(dx, dy)| {
+            img.get_pixel_mut((k.location.0 + dx) as u32, (k.location.1 + dy) as u32)
+                .0 = color;
+        });
     }
 }
--- src/orb.rs
+++ src/orb.rs
@@ -1,30 +1,39 @@
-use std::cmp::{min, max};
-use image::{ImageError, GenericImageView, DynamicImage, ImageBuffer, GrayImage, ImageFormat, RgbImage};
-use image::imageops::{blur};
-use cgmath::{prelude::{*},Rad, Deg};
 use bitvector::BitVector;
+use cgmath::{prelude::*, Deg, Rad};
+use image::{imageops::{blur, overlay}, Rgba};
+use imageproc::drawing::draw_line_segment_mut;
+use image::{
+    DynamicImage, GenericImageView, GrayImage, ImageBuffer, ImageError, ImageFormat, RgbImage,
+};
+use std::cmp::{max, min};
 
-use crate::{fast, brief, common};
-use fast::{FastKeypoint};
-use common::{*};
+use crate::{brief, common, fast};
+use common::*;
+use fast::FastKeypoint;
 
 // Consts
-const DEFAULT_BRIEF_LENGTH:usize = 256;
+const DEFAULT_BRIEF_LENGTH: usize = 256;
 
 //
 // Sobel Calculations
 //
 
-type SobelFilter = [[i32;3];3];
-const SOBEL_X : SobelFilter = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]];
-const SOBEL_Y : SobelFilter = [[1, 2, 1], [0, 0, 0], [-1, -2, -1]];
-const SOBEL_OFFSETS : [[(i32, i32);3];3] = [[(-1, -1), (0, -1), (1, -1)], [(-1, 0),  (0, 0), (1, 0)], [(-1, 1), (0, 1), (1, 1)]];
+type SobelFilter = [[i32; 3]; 3];
+const SOBEL_X: SobelFilter = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]];
+const SOBEL_Y: SobelFilter = [[1, 2, 1], [0, 0, 0], [-1, -2, -1]];
+const SOBEL_OFFSETS: [[(i32, i32); 3]; 3] = [
+    [(-1, -1), (0, -1), (1, -1)],
+    [(-1, 0), (0, 0), (1, 0)],
+    [(-1, 1), (0, 1), (1, 1)],
+];
 
 unsafe fn sobel(img: &image::GrayImage, filter: &SobelFilter, x: i32, y: i32) -> u8 {
-    let mut sobel:i32 = 0;
+    let mut sobel: i32 = 0;
     for (i, row) in filter.iter().enumerate() {
         for (j, k) in row.iter().enumerate() {
-            if *k == 0 { continue }
+            if *k == 0 {
+                continue;
+            }
 
             let offset = SOBEL_OFFSETS[i][j];
             let (x, y) = ((x + offset.0) as u32, (y + offset.1) as u32);
@@ -36,10 +45,10 @@ unsafe fn sobel(img: &image::GrayImage, filter: &SobelFilter, x: i32, y: i32) ->
 }
 
 fn create_sobel_image(img: &GrayImage) -> GrayImage {
-    let mut new_image:GrayImage = ImageBuffer::new(img.width(), img.height());
+    let mut new_image: GrayImage = ImageBuffer::new(img.width(), img.height());
 
-    for y in 1..img.height()-1 {
-        for x in 1..img.width()-1 {
+    for y in 1..img.height() - 1 {
+        for x in 1..img.width() - 1 {
             let mut px = new_image.get_pixel_mut(x, y);
             px.0[0] = unsafe { sobel(img, &SOBEL_Y, x as i32, y as i32) as u8 };
         }
@@ -56,34 +65,44 @@ fn create_sobel_image(img: &GrayImage) -> GrayImage {
 pub struct Brief {
     pub x: i32,
     pub y: i32,
-    b: BitVector
+    b: BitVector,
 }
 
 impl Matchable for Brief {
     fn distance(&self, other: &Self) -> usize {
-        (0..min(self.b.capacity(), other.b.capacity()))
-        .fold(0, |acc, x| {
+        (0..min(self.b.capacity(), other.b.capacity())).fold(0, |acc, x| {
             acc + (self.b.contains(x) != other.b.contains(x)) as usize
         })
     }
 }
 
 fn round_angle(angle: i32, increment: i32) -> i32 {
-    let modulo:i32 = angle % increment;
-    let complement:i32 = if angle < 0 
-        { increment + modulo } else { increment - modulo} ;
+    let modulo: i32 = angle % increment;
+    let complement: i32 = if angle < 0 {
+        increment + modulo
+    } else {
+        increment - modulo
+    };
 
     if modulo.abs() > (increment << 1) {
-        return if angle < 0 { angle - complement } else { angle + complement };
+        return if angle < 0 {
+            angle - complement
+        } else {
+            angle + complement
+        };
     }
 
     angle - modulo
 }
 
-pub fn brief(blurred_img: &GrayImage, vec: &Vec<FastKeypoint>, brief_length: Option<usize>) -> Vec<Brief> {
+pub fn brief(
+    blurred_img: &GrayImage,
+    vec: &Vec<FastKeypoint>,
+    brief_length: Option<usize>,
+) -> Vec<Brief> {
     let brief_length = brief_length.unwrap_or(DEFAULT_BRIEF_LENGTH);
-    let width:i32 = blurred_img.width() as i32;
-    let height:i32 = blurred_img.height() as i32;
+    let width: i32 = blurred_img.width() as i32;
+    let height: i32 = blurred_img.height() as i32;
 
     // copy offsets into current frame on stack
     let offsets = brief::OFFSETS.clone();
@@ -102,12 +121,12 @@ pub fn brief(blurred_img: &GrayImage, vec: &Vec<FastKeypoint>, brief_length: Opt
             for (i, ((x0, y0), (x1, y1))) in offsets.iter().enumerate() {
                 let mut steered_p1 = (
                     x + (x0 * cos_a - y0 * sin_a).round() as i32,
-                    y + (x0 * sin_a + y0 * cos_a).round() as i32
+                    y + (x0 * sin_a + y0 * cos_a).round() as i32,
                 );
 
                 let mut steered_p2 = (
                     x + (x1 * cos_a - y1 * sin_a).round() as i32,
-                    y + (x1 * sin_a + y1 * cos_a).round() as i32
+                    y + (x1 * sin_a + y1 * cos_a).round() as i32,
                 );
 
                 steered_p1.0 = max(min(steered_p1.0, width - 1), 0);
@@ -115,8 +134,12 @@ pub fn brief(blurred_img: &GrayImage, vec: &Vec<FastKeypoint>, brief_length: Opt
                 steered_p1.1 = max(min(steered_p1.1, height - 1), 0);
                 steered_p2.1 = max(min(steered_p2.1, height - 1), 0);
 
-                let brief_feature = blurred_img.get_pixel(steered_p1.0 as u32, steered_p1.1 as u32).0[0] >
-                                    blurred_img.get_pixel(steered_p2.0 as u32, steered_p2.1 as u32).0[0];
+                let brief_feature = blurred_img
+                    .get_pixel(steered_p1.0 as u32, steered_p1.1 as u32)
+                    .0[0]
+                    > blurred_img
+                        .get_pixel(steered_p2.0 as u32, steered_p2.1 as u32)
+                        .0[0];
 
                 if brief_feature {
                     bit_vec.insert(i);
@@ -126,8 +149,8 @@ pub fn brief(blurred_img: &GrayImage, vec: &Vec<FastKeypoint>, brief_length: Opt
             Brief {
                 x: k.location.0,
                 y: k.location.1,
-                b: bit_vec
-            } 
+                b: bit_vec,
+            }
         })
         .collect::<Vec<Brief>>()
 }
@@ -136,10 +159,10 @@ pub fn brief(blurred_img: &GrayImage, vec: &Vec<FastKeypoint>, brief_length: Opt
 // ORB Calculations
 //
 
-pub fn orb(img: &DynamicImage, n:usize) -> Result<Vec<Brief>, ImageError> {
+pub fn orb(img: &DynamicImage, n: usize) -> Result<Vec<Brief>, ImageError> {
     let gray_img = img.to_luma();
-    
-    let mut keypoints:Vec<FastKeypoint> = fast::fast(&gray_img, None, None)?;
+
+    let mut keypoints: Vec<FastKeypoint> = fast::fast(&gray_img, None, None)?;
 
     let keypoints = adaptive_nonmax_suppression(&mut keypoints, n);
 
@@ -147,4 +170,24 @@ pub fn orb(img: &DynamicImage, n:usize) -> Result<Vec<Brief>, ImageError> {
     let brief_descriptors = brief(&blurred_img, &keypoints, None);
 
     Ok(brief_descriptors)
+}
+
+pub fn draw_orb(img1: &DynamicImage, img2: &DynamicImage, key_points1: &Vec<Brief>, key_points2: &Vec<Brief>, pair_indices: &Vec<(usize, usize)>) -> DynamicImage {
+    let mut img = DynamicImage::new_rgb8(
+        img1.width() + img2.width(),
+        img1.height().max(img2.height()),
+    );
+    overlay(&mut img, img1, 0, 0);
+    overlay(&mut img, img2, img1.width(), 0);
+    pair_indices.iter().for_each(|(i1, i2)| {
+        let start_point = (key_points1[*i1].x as f32, key_points1[*i1].y as f32);
+        let end_point = (img1.width() as f32 + key_points2[*i2].x as f32, key_points2[*i2].y as f32);
+        draw_line_segment_mut(
+            &mut img,
+            start_point,
+            end_point,
+            Rgba([0, 0, 0, 125]),
+        );
+    });
+    img
 }
